85b314432112cc99de860578ed256f00f00f53cd
Christos Damianidis <chrisdamia99@gmail.com>
2025-12-08 13:53:30 +0100

    Update main.py

--- ser_api/main.py
+++ ser_api/main.py
@@ -1,26 +1,22 @@
-# ser_api/main.py
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import numpy as np
 import tensorflow as tf
 
-import tensorflow as tf  # backend
-import keras              # high-level Keras 3
+import tensorflow as tf  
+import keras              
 
 from utils.audio_utils import load_audio_from_bytes
 from utils.features import audio_to_logmel_fixed
 
 MODEL_PATH = "model/best_cnn_gru_ser.keras"
-model = keras.models.load_model(MODEL_PATH)  # <-- use keras, not tf.keras
+model = keras.models.load_model(MODEL_PATH) 
 
-
-# Ensure label order is the same as in training
 CLASS_NAMES = ["angry", "happy", "neutral", "sad"]
 
 app = FastAPI(title="Speech Emotion Recognition API")
 
-# Allow your Flutter app (you can restrict origins later)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -30,16 +26,13 @@ app.add_middleware(
 
 @app.post("/predict")
 async def predict_emotion(file: UploadFile = File(...)):
-    # Read raw bytes
     data = await file.read()
 
-    # Preprocess
     y = load_audio_from_bytes(data)
-    feat = audio_to_logmel_fixed(y)      # (128, 300, 1)
-    feat = np.expand_dims(feat, axis=0)  # (1, 128, 300, 1)
+    feat = audio_to_logmel_fixed(y)      
+    feat = np.expand_dims(feat, axis=0)  
 
-    # Predict
-    probs = model.predict(feat)[0]       # shape (4,)
+    probs = model.predict(feat)[0]     
     idx = int(np.argmax(probs))
     emotion = CLASS_NAMES[idx]
     prob = float(probs[idx])
